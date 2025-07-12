d776187866d93c67205efb1db8d52494d65118db
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2025-07-12 18:45:49 -0400

    Delete src directory

--- src/commands/request.js
+++
@@ -1,6 +0,0 @@
-import pkg from 'discord.js';
-const { SlashCommandBuilder } = pkg;
-
-export const data = new SlashCommandBuilder()
-    .setName('request')
-    .setDescription('Initiate a Disney dining alert request');
