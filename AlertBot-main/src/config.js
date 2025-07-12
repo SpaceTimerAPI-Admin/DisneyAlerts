8c30656bf9a7e544d05f156fedf556248974ede4
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2025-07-12 18:45:41 -0400

    Delete AlertBot-main directory

--- AlertBot-main/src/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-export const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-export const GUILD_ID = process.env.DISCORD_GUILD_ID;
-export const DISNEY_EMAIL = process.env.DISNEY_EMAIL;
-export const DISNEY_PASSWORD = process.env.DISNEY_PASSWORD;
